d349e627675636db74aa1340090a18342ccff254
Shubhankar Saxena <saxena03121999@gmail.com>
2020-10-13 00:30:44 +0530

    Updates

---
+++ precache-manifest.27a8a6abbb79be16c25226c316969624.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c12b2f0b7033a18140575a77c7005f4",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "4b319ca58871cfcb2368",
+    "url": "/react-gh-pages/static/css/main.4f5cd613.chunk.css"
+  },
+  {
+    "revision": "ff5b9b027e76769f3d0f",
+    "url": "/react-gh-pages/static/js/2.c7ea1c75.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/react-gh-pages/static/js/2.c7ea1c75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b319ca58871cfcb2368",
+    "url": "/react-gh-pages/static/js/main.2574c9e7.chunk.js"
+  },
+  {
+    "revision": "77607358f4407843ab81",
+    "url": "/react-gh-pages/static/js/runtime-main.5d6d6194.js"
+  }
+]);
